6d67e9a86282bb52e8c0bd358ed2144c44c9c25d
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-10-02 17:01:08 -0300

    Created resource for updating and deleting user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,7 +8,7 @@ module.exports = {
     },
 
     async store(req, res) {
-        const {name, email} = req.body
+        const { name, email } = req.body
 
         const emailFormatted = email.toLowerCase()
 
@@ -18,5 +18,58 @@ module.exports = {
         })
 
         return res.json(user)
+    },
+
+    async update(req, res) {
+        const { user_id } = req.params
+        const { name, email } = req.body
+
+        const user = await User.findByPk(user_id)
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' })
+        }
+
+        if (email == user.email) {
+            return res.status(400).json({ error: 'Email inserted is the same registered' })
+        }
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email }
+            })
+
+            if (userExists) {
+                return res.status(400).json({ error: 'Email already exists in the database' })
+            }
+        }
+
+        await user.update(req.body)
+
+        return res.json({
+            error: false,
+            message: 'Data updated successfully',
+            name,
+            email
+        })
+    },
+
+    async delete(req, res) {
+        const { user_id } = req.params
+
+        const user = await User.findByPk(user_id)
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' })
+        }
+
+        await User.destroy({
+            user,
+            where: {
+                id: user_id
+            }
+        })
+
+        return res.json()
     }
 }
